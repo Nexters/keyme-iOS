19b12fc80790280f55fa634a305b13c6fba283d7
enebin <enebin777@gmail.com>
2023-07-16 18:54:21 +0900

    chore: Add Moya to dependencies

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -10,7 +10,8 @@ import Foundation
 import ProjectDescription
 let dependencies = Dependencies(
       swiftPackageManager: [
-          .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .upToNextMajor(from: "10.11.0"))
+          .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .upToNextMajor(from: "10.11.0")),
+          .remote(url: "https://github.com/Moya/Moya", requirement: .upToNextMajor(from: "15.0.3"))
       ],
        platforms: [.iOS]
 )
