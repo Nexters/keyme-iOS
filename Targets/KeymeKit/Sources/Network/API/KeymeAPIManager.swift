274babccc66c48a284d478fc6e862350a1a70e5c
enebin <enebin777@gmail.com>
2023-07-18 22:28:34 +0900

    refactor: Remove default implementation

--- Targets/KeymeKit/Sources/Network/API/KeymeAPIManager.swift
+++ Targets/KeymeKit/Sources/Network/API/KeymeAPIManager.swift
@@ -11,15 +11,43 @@ import CombineMoya
 import Moya
 import Foundation
 
-public struct KeymeAPIManager: CoreNetworking, APIRequestable {
-    var core: CoreNetworkService<KeymeAPI>
-    var provider: MoyaProvider<KeymeAPI> {
-        core.provider
-    }
-    let decoder = JSONDecoder()
+public struct KeymeAPIManager {
+    public typealias APIType = KeymeAPI
+    
+    private var core: CoreNetworkService<KeymeAPI>
+    private let decoder = JSONDecoder()
     
     init(core: CoreNetworkService<KeymeAPI>) {
         self.core = core
+    }
+    
+    @discardableResult
+    public mutating func registerAuthorizationToken(_ token: String) -> Self {
+        core.registerAuthorizationToken(token)
+        return self
+    }
+}
+
+extension KeymeAPIManager: CoreNetworking {
+    public func request(_ api: KeymeAPI) async throws -> Response {
+        try await core.request(api)
+    }
+    
+    public func request(_ api: KeymeAPI) -> AnyPublisher<Response, MoyaError> {
+        core.request(api)
+    }
+}
+
+extension KeymeAPIManager: APIRequestable {
+    public func request<T: Decodable>(_ api: KeymeAPI, object: T.Type) async throws -> T {
+        let response = try await core.request(api)
+        let decoded = try decoder.decode(T.self, from: response.data)
+        
+        return decoded
+    }
+    
+    public func request<T: Decodable>(_ api: KeymeAPI, object: T.Type) -> AnyPublisher<T, MoyaError> {
+        core.request(api).map(T.self)
     }
 }
 
