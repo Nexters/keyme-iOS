7c209cb1eaff124151ee7ad98b907390bdf814fc
김영인 <0inn1220@gmail.com>
2023-07-26 12:30:15 +0900

    feature #10: XCConfig 추가 및 암호화 적용

--- Scripts/decrypt.sh
+++ Scripts/decrypt.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 KEY_PATH="Tuist/master.key"
-SECRETS_DIR="Targets/Keyme/Encrypted"
-OUTPUT_DIR="Targets/Keyme/Resources/Secrets"
+
+SECRETS_DIR="Encrypted"
+OUTPUT_DIR="Projects/Keyme/Resources/Secrets"
+
+XCCONFIG_APP_DIR="XCConfig/App"
+XCCONFIG_TARGET_DIR="XCConfig/Target"
 
 # Find all encrypted files in the secrets directory
 files=($(find ${SECRETS_DIR} -type f -name "*.encrypted"))
@@ -13,8 +17,16 @@ do
    # Extract the filename from the file path
    filename=$(basename $file .encrypted)
 
+   if [[ $file == *"$XCCONFIG_APP_DIR"* ]]; then
+        out_dir="${XCCONFIG_APP_DIR}"
+   elif [[ $file == *"$XCCONFIG_TARGET_DIR"* ]]; then
+        out_dir="${XCCONFIG_TARGET_DIR}"
+   else
+        out_dir="${OUTPUT_DIR}"
+   fi
+   
    # Create the output file path by appending the filename to the output directory
-   out_file="${OUTPUT_DIR}/${filename}"
+   out_file="${out_dir}/${filename}"
 
    # Only decrypt if the normal file does not exist
    if [ ! -f "$out_file" ]; then
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -5,13 +5,17 @@
 //  Created by 이영빈 on 2023/07/14.
 //
 
-import Foundation
-
 import ProjectDescription
+import ProjectDescriptionHelpers
+
+import ConfigPlugin
+
 let dependencies = Dependencies(
-      swiftPackageManager: [
-          .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .upToNextMajor(from: "10.11.0")),
-          .remote(url: "https://github.com/Moya/Moya", requirement: .upToNextMajor(from: "15.0.3"))
-      ],
-       platforms: [.iOS]
+    swiftPackageManager: SwiftPackageManagerDependencies(
+        [
+            .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .upToNextMajor(from: "10.11.0")),
+            .remote(url: "https://github.com/Moya/Moya", requirement: .upToNextMajor(from: "15.0.3"))
+        ],
+        baseSettings: .settings(configurations: XCConfig.framework)
+    )
 )
