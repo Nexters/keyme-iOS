0001f34ffbbc79db5e14f63fba9e53317367134c
김영인 <0inn1220@gmail.com>
2023-07-30 18:24:26 +0900

    feature #17: TestFeature 구현
    TestStore, TestView 구현

---
+++ Projects/Features/Sources/Test/TestView.swift
@@ -0,0 +1,32 @@
+//
+//  TestView.swift
+//  Features
+//
+//  Created by 김영인 on 2023/07/29.
+//  Copyright © 2023 team.humanwave. All rights reserved.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+public struct TestView: View {
+    public let store: StoreOf<TestStore>
+    
+    public init(store: StoreOf<TestStore>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            
+            VStack(spacing: 50) {
+                Text(viewStore.text ?? "")
+                
+                Button("테스트 서버 호출하기") {
+                    viewStore.send(.buttonDidTap)
+                }
+            }
+        }
+    }
+}
