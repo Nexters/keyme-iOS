6f828ecbcf76df928af33fe1b1c8afc99e1557d5
Young Bin Lee <enebin777@gmail.com>
2023-08-10 15:08:36 +0900

    refactor #28: 구조를 TCA스럽게 변경

---
+++ Projects/Features/Sources/KeymeServiceStatus/SignIn.swift
@@ -0,0 +1,28 @@
+//
+//  SignIn.swift
+//  Features
+//
+//  Created by 이영빈 on 2023/08/10.
+//  Copyright © 2023 team.humanwave. All rights reserved.
+//
+
+import Foundation
+import ComposableArchitecture
+
+public struct SignInFeature: Reducer {
+    public enum State: Equatable {
+        case loggedIn
+        case loggedOut
+    }
+    
+    public enum Action: Equatable {
+        case succeeded
+        case failed
+    }
+    
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
--- Projects/Keyme/Sources/KeymeApp.swift
+++ Projects/Keyme/Sources/KeymeApp.swift
@@ -1,66 +1,16 @@
 import SwiftUI
-import SwiftUIIntrospect
 import UserNotifications
 
-import ComposableArchitecture
 import FirebaseCore
 import FirebaseMessaging
-
-import Features
 
 @main
 struct KeymeApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @AppStorage("isNewbie") private var isNewbie = true
-    @State private var selectedTab = 0
-
+    
     var body: some Scene {
         WindowGroup {
-            if isNewbie {
-                // 온보딩은 여기에
-                Toggle(isOn: $isNewbie) {
-                    Text("뉴비세요?")
-                }
-                .frame(width: 150)
-            } else {
-                // 온보딩 끝난 후 메인페이지 진입
-                TabView(selection: $selectedTab) {
-                    TestView(store: Store(
-                        initialState: TestStore.State(),
-                        reducer: TestStore()))
-                    .tabItem {
-                        Image(systemName: "1.square.fill")
-                        Text("Home")
-                    }
-                    .tag(0)
-                    
-                    Text("Tab 2 Content")
-                        .tabItem {
-                            Image(systemName: "2.square.fill")
-                            Text("My page")
-                        }
-                        .tag(1)
-                }
-                .introspect(.tabView, on: .iOS(.v16, .v17)) { tabViewController in
-                    let tabBar = tabViewController.tabBar
-                    
-                    let barAppearance = UITabBarAppearance()
-                    barAppearance.configureWithOpaqueBackground()
-                    barAppearance.backgroundColor = .black
-                    
-                    let itemAppearance = UITabBarItemAppearance()
-                    itemAppearance.selected.iconColor = .white
-                    itemAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
-                    itemAppearance.normal.iconColor = .gray
-                    itemAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.gray]
-                    
-                    tabBar.standardAppearance = barAppearance
-                    tabBar.standardAppearance.inlineLayoutAppearance = itemAppearance
-                    tabBar.standardAppearance.stackedLayoutAppearance = itemAppearance
-                    tabBar.standardAppearance.compactInlineLayoutAppearance = itemAppearance
-                    tabBar.scrollEdgeAppearance = tabBar.standardAppearance
-                }
-            }
+            RootView()
         }
     }
 }
