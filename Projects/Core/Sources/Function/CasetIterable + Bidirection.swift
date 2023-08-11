06cc058d631a46fa7e91d63877c51d2535b02382
Young Bin Lee <enebin777@gmail.com>
2023-08-11 16:12:15 +0900

    feat #44: `CasetIterable`에 다음, 이전 케이스 탐색 메서드 추가

---
+++ Projects/Core/Sources/Function/CasetIterable + Bidirection.swift
@@ -0,0 +1,37 @@
+//
+//  BidirectionalCasetIterable.swift
+//  Core
+//
+//  Created by 이영빈 on 2023/08/11.
+//  Copyright © 2023 team.humanwave. All rights reserved.
+//
+
+import Foundation
+
+extension CaseIterable where Self: Equatable, AllCases: BidirectionalCollection {
+    /// 이전 `case`를 반환해요
+    func previous() -> Self {
+        let all = Self.allCases
+        let idx = all.firstIndex(of: self)!
+        let previous = all.index(before: idx)
+        
+        if idx == all.startIndex {
+            return self
+        } else {
+            return all[previous]
+        }
+    }
+
+    /// 다음 `case`를 반환해요
+    func next() -> Self {
+        let all = Self.allCases
+        let idx = all.firstIndex(of: self)!
+        let next = all.index(after: idx)
+        
+        if next == all.endIndex {
+            return self
+        } else {
+            return all[next]
+        }
+    }
+}
