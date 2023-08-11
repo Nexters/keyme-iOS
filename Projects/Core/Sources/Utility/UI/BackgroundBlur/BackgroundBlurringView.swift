4808beb506c9dd7a62b97fc9c4cffaf87b6edbff
Young Bin Lee <enebin777@gmail.com>
2023-08-11 11:58:13 +0900

    chore: 메서드 접근 한정자 조정

--- Projects/Core/Sources/Utility/UI/BackgroundBlur/BackgroundBlurringView.swift
+++ Projects/Core/Sources/Utility/UI/BackgroundBlur/BackgroundBlurringView.swift
@@ -8,14 +8,19 @@
 
 import SwiftUI
 
-struct BackgroundBlurringView: UIViewRepresentable {
-    var style: UIBlurEffect.Style
+/// 이 뷰를 만들면 이 뷰 뒷배경이 다 블러처리 된답니다
+public struct BackgroundBlurringView: UIViewRepresentable {
+    public var style: UIBlurEffect.Style
+    
+    public init(style: UIBlurEffect.Style) {
+        self.style = style
+    }
 
-    func makeUIView(context: UIViewRepresentableContext<BackgroundBlurringView>) -> UIVisualEffectView {
+    public func makeUIView(context: UIViewRepresentableContext<BackgroundBlurringView>) -> UIVisualEffectView {
         UIVisualEffectView(effect: UIBlurEffect(style: style))
     }
 
-    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<BackgroundBlurringView>) {
+    public func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<BackgroundBlurringView>) {
         uiView.effect = UIBlurEffect(style: style)
     }
 }
