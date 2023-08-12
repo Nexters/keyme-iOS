687f81b549cf0e8e4c0b527b12424709f9810a6d
k906506 <k906506@naver.com>
2023-08-12 17:08:16 +0900

    feature #20: TextField가 좌, 우로 떨리는 애니메이션 구현

--- Projects/Features/Sources/Privacy/Modifiers.swift
+++ Projects/Features/Sources/Privacy/Modifiers.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2023 team.humanwave. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+// 최대 글자 수를 넘기면 좌, 우로 떨리는 애니메이션
+struct ShakeTextFieldWhenIsFulled: ViewModifier {
+    @Binding var isShake: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .offset(x: isShake ? -10 : 0) // 좌측으로 이동
+            .animation(
+                Animation
+                    .easeInOut(duration: 0.1)
+                    .repeatCount(3, autoreverses: true),
+                value: isShake)
+            .onChange(of: isShake) { newValue in
+                if !newValue {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        withAnimation {
+                            self.isShake = false
+                        }
+                    }
+                }
+            }
+    }
+}
