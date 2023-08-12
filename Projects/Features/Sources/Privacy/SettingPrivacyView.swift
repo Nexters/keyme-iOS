2fcf739cebe83768859796e777de338cc8ab59e5
k906506 <k906506@naver.com>
2023-08-12 20:07:40 +0900

    feature #20: 프로필 이미지 등록 구현

--- Projects/Features/Sources/Privacy/SettingPrivacyView.swift
+++ Projects/Features/Sources/Privacy/SettingPrivacyView.swift
@@ -7,28 +7,88 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 // FIXME: 폰트, 컬러(도형, 폰트) 추후에 한 번에 변경 예정
 // FIXME: 텍스트도 Constants로 변경 예정
+/// 회원가입 이후, 프로필 이미지와 닉네임을 등록하는 페이지입니다.
+/// - 뷰의 상단부터 순차적으로 구현하였으며 각각의 컴포넌트별로 구분할 수 있게끔 주석을 달아놓았으니 참고하시면 됩니다.
 struct SettingPrivacyView: View {
+    // 닉네임 관련 프로퍼티
     @State private var nickname = "" // 사용자가 새롭게 입력한 닉네임
-    @State private var beforeNickname = "" // 기존에 입력했던 닉네임
-    @State private var isValidNickname = false // 사용 가능한 닉네임인지?
-    @State private var isShake = false // 최대 글자 수를 넘긴 경우 좌, 우로 떨리는 애니메이션
-    @State private var isToggle = false // 다음 페이지로 넘어가기
+    @State private var beforeNickname = "" // 기존에 입력한 닉네임
+    @State private var isValidNickname = false
+    @State private var isShake = false // TextField를 좌, 우로 Shake
+    @State private var isToggle = false // 다음 페이지로 넘어가기 위한 버튼
+    
+    // 프로필 이미지 관련 프로퍼티들
+    @State private var selectedImage: PhotosPickerItem? = nil // 사용자 프로필 이미지
+    @State private var selectedImageData: Data? = nil // Image -> Data
     
     var body: some View {
-        VStack {
+        VStack(spacing: 12) {
             // 프로필 이미지를 등록하는 Circle
-            Circle()
-                .frame(width: 160, height: 160)
-                .foregroundColor(.gray)
-                .overlay(
-                    Circle()
-                        .stroke(.white, lineWidth: 1)
-                )
-                .padding(32)
+            PhotosPicker(
+                selection: $selectedImage,
+                matching: .images,
+                photoLibrary: . shared()) {
+                    if let selectedImageData, let profileImage = UIImage(data: selectedImageData) {
+                        Image(uiImage: profileImage)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 160, height: 160)
+                            .clipShape(Circle())
+                            .overlay(
+                                ZStack {
+                                    Circle()
+                                        .foregroundColor(.gray)
+                                        .frame(width: 50, height: 50)
+                                    
+                                    Image(systemName: "photo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 33.3, height: 33.3)
+                                        .foregroundColor(.white)
+                                }
+                                .offset(x: 50, y: 50)
+                            )
+                            .padding(32)
+                    } else {
+                        Circle()
+                            .frame(width: 160, height: 160)
+                            .foregroundColor(.gray)
+                            .overlay(
+                                Circle()
+                                    .stroke(.white, lineWidth: 1)
+                            )
+                            .overlay(
+                                ZStack {
+                                    Circle()
+                                        .foregroundColor(.gray)
+                                        .frame(width: 50, height: 50)
+                                    
+                                    Image(systemName: "photo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 33.3, height: 33.3)
+                                        .foregroundColor(.white)
+                                }
+                                .offset(x: 50, y: 50)
+                            )
+                            .padding(32)
+                    }
+                }
             
+                // Image -> Data
+                .onChange(of: selectedImage) { newImage in
+                    Task {
+                        if let data = try await newImage?.loadTransferable(type: Data.self) {
+                            selectedImageData = data
+                        }
+                    }
+                }
+            
+            // 닉네임 관련 안내메세지
             HStack(alignment: .center, spacing: 4) {
                 Text("닉네임")
                     .font(.system(size: 14))
@@ -71,28 +131,28 @@ struct SettingPrivacyView: View {
                         .foregroundColor(.white)
                         .padding(.horizontal, 8)
                 )
-                .padding(.top, 8)
             
             // 위쪽 정렬을 위한 Spacer
             Spacer()
             
             // 다음 페이지로 넘어가기 위한 Button
             // FIXME: API 연결
-            Button(action: { }) {
+            Button(action: {}) {
                 HStack {
                     Spacer()
                     
                     Text("다음")
-                        .foregroundColor(.white)
+                        .font(.system(size: 18))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
                         .frame(height: 60)
                     
                     Spacer()
                 }
             }
-            .background(isToggle ? .black : .gray)
+            .background(isToggle ? .white : .gray)
             .cornerRadius(16)
             .disabled(isToggle ? false : true)
-            .padding(.bottom)
         }
         .padding(.horizontal, 16)
         
@@ -130,9 +190,10 @@ struct ValidateNicknameView: View {
             
             Spacer()
         }
-        .padding([.top, .horizontal], 8)
+        .padding(8)
     }
 }
+
 struct SettingPrivacyView_Previews: PreviewProvider {
     static var previews: some View {
         SettingPrivacyView()
