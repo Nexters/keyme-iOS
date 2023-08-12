75b7457399bdcf6fbd2ad4b4055454570b929cd6
김영인 <0inn1220@gmail.com>
2023-08-12 20:52:05 +0900

    chore #46: 예시 코드 추가

--- Projects/Features/Sources/Test/TestView.swift
+++ Projects/Features/Sources/Test/TestView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 import ComposableArchitecture
 
+import DSKit
+
 public struct TestView: View {
     public let store: StoreOf<TestStore>
     
@@ -21,6 +23,11 @@ public struct TestView: View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             VStack(spacing: 50) {
                 Text(viewStore.text ?? "")
+                    .font(Font.Keyme.body2)
+                    .foregroundColor(DSKitAsset.Color.keymeBlack.swiftUIColor)
+                
+                DSKitAsset.Image.camera.swiftUIImage
+                    .frame(width: 40, height: 40)
                 
                 Button("테스트 서버 호출하기") {
                     viewStore.send(.buttonDidTap)
