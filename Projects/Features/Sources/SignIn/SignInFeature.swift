137fb71c10f559cd539e3a16eb785dfd761ebb3e
k906506 <k906506@naver.com>
2023-08-15 22:19:04 +0900

    feat #19: 카카오 로그인 Keyme API와 연결

--- Projects/Features/Sources/SignIn/SignInFeature.swift
+++ Projects/Features/Sources/SignIn/SignInFeature.swift
@@ -12,6 +12,7 @@ import AuthenticationServices
 import ComposableArchitecture
 import Foundation
 import KakaoSDKUser
+import Network
 
 public struct SignInFeature: Reducer {
     @Dependency(\.localStorage) private var localStorage
@@ -58,7 +59,7 @@ public struct SignInFeature: Reducer {
             case .signInWithKakaoResponse(.failure): // 카카오 로그인 실패
                 state = .loggedOut
                 return .none
-            
+                
             case .signInWithApple:
                 signInWithApple()
                 
@@ -87,13 +88,12 @@ public struct SignInFeature: Reducer {
 }
 
 extension SignInFeature {
-    // FIXME: 토큰 검증 로직 추가
     // 카카오 로그인 메서드
     /// Reducer Closure 내부에서 State를 직접 변경할 수 없어서 Async - Await를 활용하여 한 번 더 이벤트(signInWithKakaoResponse)를 발생시키도록 구현했습니다.
     private func signInWithKakao() async throws -> Bool {
         return try await withCheckedThrowingContinuation { continuation in
             if (UserApi.isKakaoTalkLoginAvailable()) {
-                UserApi.shared.loginWithKakaoTalk { (_, error) in
+                UserApi.shared.loginWithKakaoTalk { (token, error) in
                     if let error = error {
                         continuation.resume(throwing: SignInError.noSignIn)
                     } else {
@@ -101,11 +101,33 @@ extension SignInFeature {
                     }
                 }
             } else {
-                UserApi.shared.loginWithKakaoAccount() { (_, error) in
+                UserApi.shared.loginWithKakaoAccount() { (data, error) in
                     if let error = error {
                         continuation.resume(throwing: SignInError.noSignIn)
                     } else {
-                        continuation.resume(returning: true)
+                        do {
+                            // 1. 카카오 API로 사용자 정보 가져오기
+                            let jsonData = try JSONEncoder().encode(data)
+                            let parsedData = try JSONDecoder().decode(KakaoOAuthResponse.self, from: jsonData)
+                            
+                            // 2. Keyme API로 사용자 토큰 확인하기
+                            let auth = KeymeOAuthRequest(oauthType: "KAKAO", token: parsedData.accessToken)
+                            Task {
+                                do {
+                                    let result = try await KeymeAPIManager.shared.request(.auth(param: auth), object: KeymeOAuthResponse.self)
+                                    
+                                    if result.code == 200 {
+                                        return continuation.resume(returning: true)
+                                    } else {
+                                        return continuation.resume(throwing: SignInError.noSignIn)
+                                    }
+                                } catch { // 에러가 발생하면 실패 처리
+                                    return continuation.resume(throwing: SignInError.noSignIn)
+                                }
+                            }
+                        } catch { // 에러가 발생하면 실패 처리
+                            continuation.resume(throwing: SignInError.noSignIn)
+                        }
                     }
                 }
             }
---
+++ Projects/Network/Sources/DTO/Auth.swift
@@ -0,0 +1,56 @@
+//
+//  Auth.swift
+//  Network
+//
+//  Created by 고도현 on 2023/08/15.
+//  Copyright © 2023 team.humanwave. All rights reserved.
+//
+
+import Foundation
+
+public struct KakaoOAuthResponse: Codable { // 카카오 API Response 관련 Model
+    public let refreshTokenExpiresIn: Int
+    public let tokenType: String
+    public let refreshToken: String
+    public let accessToken: String
+    public let expiresIn: Int
+    public let scope: String
+    
+    public init(refreshTokenExpiresIn: Int, tokenType: String, refreshToken: String, accessToken: String, expiresIn: Int, scope: String) {
+        self.refreshTokenExpiresIn = refreshTokenExpiresIn
+        self.tokenType = tokenType
+        self.refreshToken = refreshToken
+        self.accessToken = accessToken
+        self.expiresIn = expiresIn
+        self.scope = scope
+    }
+}
+
+public struct KeymeOAuthRequest: Codable { // KeyMe API Request 관련 Model
+    public let oauthType: String
+    public let token: String
+    
+    public init(oauthType: String, token: String) {
+        self.oauthType = oauthType
+        self.token = token
+    }
+}
+
+public struct KeymeOAuthResponse: Codable { // KeyMe API Response 관련 Model
+    public let code: Int // statusCode
+    public let data: Data
+    public let message: String
+    
+    public struct Data: Codable {
+        public let id: Int
+        public let friendCode: String?
+        public let nickname: String?
+        public let profileImage: String?
+        public let profileTumbnail: String?
+        public let token: Token
+        
+        public struct Token: Codable {
+            public let accessToken: String
+        }
+    }
+}
