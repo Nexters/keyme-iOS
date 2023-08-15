3fba7380f9b42f718770867eb87fc0bc632d1bc9
k906506 <k906506@naver.com>
2023-08-15 10:48:33 +0900

    feat #19: 애플 로그인 기능 구현

--- Projects/Features/Sources/SignIn/SignInFeature.swift
+++ Projects/Features/Sources/SignIn/SignInFeature.swift
@@ -8,12 +8,15 @@
 //  Copyright © 2023 team.humanwave. All rights reserved.
 //
 
-import Foundation
+import AuthenticationServices
 import ComposableArchitecture
+import Foundation
 import KakaoSDKUser
 
 public struct SignInFeature: Reducer {
     @Dependency(\.localStorage) private var localStorage
+    
+    private let signInWithAppleDelegate = SignInWithAppleDelegate()
     
     public enum SignInError: Error {
         case noSignIn
@@ -26,16 +29,19 @@ public struct SignInFeature: Reducer {
     }
     
     public enum Action: Equatable {
-        case signIn
+        case signInWithKakao
         case signInWithKakaoResponse(TaskResult<Bool>)
-//        case succeeded
-//        case failed
+        
+        case signInWithApple
+        case signInWithAppleResponse(TaskResult<Bool>)
+        //        case succeeded
+        //        case failed
     }
     
     public var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
-            case .signIn:
+            case .signInWithKakao:
                 return .run { send in
                     await send(.signInWithKakaoResponse(
                         TaskResult {
@@ -43,18 +49,44 @@ public struct SignInFeature: Reducer {
                         }
                     ))
                 }
+                
             case .signInWithKakaoResponse(.success(true)): // 카카오 로그인 성공
                 state = .loggedIn
                 localStorage.set(true, forKey: .isLoggedIn)
+                return .none
+                
             case .signInWithKakaoResponse(.failure): // 카카오 로그인 실패
                 state = .loggedOut
+                return .none
+            
+            case .signInWithApple:
+                signInWithApple()
+                
+                if signInWithAppleDelegate.isLoggedIn {
+                    return Effect.send(.signInWithAppleResponse(.success(true)))
+                } else {
+                    return Effect.send(.signInWithAppleResponse(.failure(SignInError.noSignIn)))
+                }
+                
+            case .signInWithAppleResponse(.success(true)): // 애플 로그인 성공
+                state = .loggedIn
+                localStorage.set(true, forKey: .isLoggedIn)
+                return .none
+                
+            case .signInWithAppleResponse(.failure): // 애플 로그인 실패
+                state = .loggedOut
+                return .none
+                
             default:
                 state = .loggedOut
             }
+            
             return .none
         }
     }
-    
+}
+
+extension SignInFeature {
     // FIXME: 토큰 검증 로직 추가
     // 카카오 로그인 메서드
     /// Reducer Closure 내부에서 State를 직접 변경할 수 없어서 Async - Await를 활용하여 한 번 더 이벤트(signInWithKakaoResponse)를 발생시키도록 구현했습니다.
@@ -78,5 +110,14 @@ public struct SignInFeature: Reducer {
                 }
             }
         }
+    }
+    
+    private func signInWithApple() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.email, .fullName]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = signInWithAppleDelegate
+        controller.performRequests()
     }
 }
