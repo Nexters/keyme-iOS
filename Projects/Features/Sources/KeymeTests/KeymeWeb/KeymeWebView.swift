7cb0af8eb245a329f77f11cb7f47838d4a1be278
김영인 <0inn1220@gmail.com>
2023-08-17 04:41:09 +0900

    feat #25: WebView 기본 세팅

---
+++ Projects/Features/Sources/KeymeTests/KeymeWeb/KeymeWebView.swift
@@ -0,0 +1,44 @@
+//
+//  KeymeWebView.swift
+//  Features
+//
+//  Created by 김영인 on 2023/08/17.
+//  Copyright © 2023 team.humanwave. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+public struct KeymeWebView: UIViewRepresentable {
+    public let url: String
+    
+    init(url: String) {
+        self.url = url
+    }
+    
+    public func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView(frame: CGRect.zero, configuration:  WKWebViewConfiguration())
+        
+        if let url = URL(string: url) {
+            webView.load(URLRequest(url: url))
+        }
+        
+        return webView
+    }
+    
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
+
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    public class Coordinator: NSObject {
+        let parent: KeymeWebView
+        
+        init(parent: KeymeWebView) {
+            self.parent = parent
+        }
+    }
+}
