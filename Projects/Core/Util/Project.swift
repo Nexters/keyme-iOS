1de8695f802acf3804264715586334bc19016e4d
enebin <enebin777@gmail.com>
2023-08-17 20:46:14 +0900

    feat: 의존성 수정

--- Projects/Core/Util/Project.swift
+++ Projects/Core/Util/Project.swift
@@ -9,5 +9,8 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
-    name: "Util"
+    name: "Util",
+    externalDependencies: [
+        .external(name: "Kingfisher")
+    ]
 )
