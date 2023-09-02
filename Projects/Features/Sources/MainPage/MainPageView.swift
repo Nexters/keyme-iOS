1ad62f2327c3291a84f9c354801ef52334ab4f79
enebin <enebin777@gmail.com>
2023-09-03 08:53:23 +0900

    feature #89: 네비게이션 바 커스텀

--- Projects/Features/Sources/MainPage/MainPageView.swift
+++ Projects/Features/Sources/MainPage/MainPageView.swift
@@ -60,6 +60,38 @@ struct KeymeMainView: View {
                     tabBar.scrollEdgeAppearance = tabBar.standardAppearance
                 }
             }
+            .navigationBarHidden(true)
+        }
+        .introspect(.navigationStack, on: .iOS(.v16, .v17)) { navigationController in
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.backgroundColor = UIColor(DSKitAsset.Color.keymeBlack.swiftUIColor)
+            appearance.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor.white
+            ]
+
+            let backbuttonImage = UIImage(systemName: "chevron.left")?
+                .withRenderingMode(.alwaysOriginal)
+                .withTintColor(.white)
+                .withAlignmentRectInsets(UIEdgeInsets(top: 0.0, left: -12.0, bottom: 0, right: 0.0))
+            
+            appearance.setBackIndicatorImage(backbuttonImage, transitionMaskImage: backbuttonImage)
+            
+            let backButtonAppearance = UIBarButtonItemAppearance()
+               // backButton하단에 표출되는 text를 안보이게 설정
+            backButtonAppearance.normal.titleTextAttributes = [
+                .foregroundColor: UIColor.clear,
+                .font: UIFont.systemFont(ofSize: 0.0)
+            ]
+            
+            appearance.backButtonAppearance = backButtonAppearance
+
+            navigationController.navigationBar.barTintColor = .white
+            navigationController.navigationBar.tintColor = .white
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.compactAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+            navigationController.navigationBar.isTranslucent = false
         }
     }
 }
--- Projects/Features/Sources/Setting/SettingView.swift
+++ Projects/Features/Sources/Setting/SettingView.swift
@@ -7,7 +7,9 @@
 //
 
 import ComposableArchitecture
+import DSKit
 import SwiftUI
+import SwiftUIIntrospect
 
 struct SettingView: View {
     private let store: StoreOf<SettingFeature>
@@ -17,29 +19,64 @@ struct SettingView: View {
     }
     
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            List {
-                Section("개인정보") {
-                    Button("로그아웃") {
-                        
-                    }
-                    
-                    Button("서비스 탈퇴") {
-                        
-                    }
-                }
+        WithViewStore(store, observe: { $0 }) { _ in
+            ZStack {
+                DSKitAsset.Color.keymeBlack.swiftUIColor
+                    .ignoresSafeArea()
                 
-                Section("마케팅 정보 수신 동의") {
-                    
-                    HStack {
-                        Text("푸시 알림")
-                        Spacer()
-                        Toggle("", isOn: .constant(true))
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 30) {
+                        section(title: "개인정보") {
+                            Button(action: {}) {
+                                item(text: "로그아웃")
+                            }
+                            Button(action: {}) {
+                                item(text: "서비스 탈퇴")
+                            }
+                        }
+                        
+                        Divider()
+                        
+                        section(title: "마케팅 정보 수신 동의") {
+                            HStack {
+                                item(text: "푸시 알림")
+                                
+                                Spacer()
+                                
+                                Toggle("", isOn: .constant(true))
+                            }
+                        }
                     }
+                    .fullFrame()
+                    .foregroundColor(.white)
+                    .scrollContentBackground(.hidden)
+                    .padding(.horizontal, 34)
+                    .padding(.top, 40)
                 }
             }
-            .listStyle(.inset)
-            .background(.red)
         }
+    }
+}
+
+private extension SettingView {
+    func section(title: String, @ViewBuilder items: () -> some View) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            caption(text: title)
+                .padding(.bottom, 36)
+            
+            VStack(alignment: .leading, spacing: 24) {
+                items()
+            }
+        }
+    }
+    
+    func caption(text: String) -> some View {
+        Text.keyme(text, font: .body4)
+            .foregroundColor(DSKitAsset.Color.keymeMediumgray.swiftUIColor)
+    }
+    
+    func item(text: String) -> some View {
+        Text.keyme(text, font: .body2)
+            .foregroundColor(DSKitAsset.Color.keymeWhite.swiftUIColor)
     }
 }
