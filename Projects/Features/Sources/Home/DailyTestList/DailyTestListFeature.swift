ab62c9972ad1af5550ada00acb2e8f98e361f324
김영인 <0inn1220@gmail.com>
2023-09-06 22:28:15 +0900

    feat #93: 데일리 리스트 feature 구현

---
+++ Projects/Features/Sources/Home/DailyTestList/DailyTestListFeature.swift
@@ -0,0 +1,56 @@
+//
+//  DailyTestFeature.swift
+//  Features
+//
+//  Created by 김영인 on 2023/09/06.
+//  Copyright © 2023 team.humanwave. All rights reserved.
+//
+
+import ComposableArchitecture
+
+import Domain
+import Network
+
+public struct DailyTestListFeature: Reducer {
+    @Dependency(\.keymeAPIManager) private var network
+
+    public struct State: Equatable {
+        let testData: KeymeTestsModel
+        var dailyStatistics: DailyStatisticsModel = .EMPTY
+        
+        init(testData: KeymeTestsModel) {
+            self.testData = testData
+        }
+    }
+    
+    public enum Action {
+        case viewWillAppear
+        case fetchDailyStatistics
+        case saveDailyStatistics(DailyStatisticsModel)
+        case shareButtonDidTap
+    }
+    
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .viewWillAppear:
+                return .send(.fetchDailyStatistics)
+            case .fetchDailyStatistics:
+                return .run { [testId = state.testData.testId] send in
+//                    let dailyStatisticsData = try await network.request(.test(.statistics(testId)), object: StatisticsDTO.self)
+                    let dailyStatisticsData = try await network.requestWithSampleData(.test(.statistics(testId)), object: StatisticsDTO.self)
+                    let dailyStatistics = dailyStatisticsData.toDailyStatisticsModel()
+                    await send(.saveDailyStatistics(dailyStatistics))
+                }
+                
+            case let .saveDailyStatistics(dailyStatistics):
+                state.dailyStatistics = dailyStatistics
+                
+            default:
+                return .none
+            }
+            
+            return .none
+        }
+    }
+}
