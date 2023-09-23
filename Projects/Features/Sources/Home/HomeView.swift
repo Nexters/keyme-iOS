393bac7e881d9980de8dae5eb12ff2e3120dbe3f
YoungBin Lee <enebin777@gmail.com>
2023-09-23 22:12:15 +0900

    refactor : 홈 뷰 디자인 명세 충족

--- Projects/Features/Sources/Home/HomeView.swift
+++ Projects/Features/Sources/Home/HomeView.swift
@@ -39,6 +39,13 @@ public struct HomeView: View {
                             Spacer().frame(height: 60) // 아래 공간 띄우기
                         }
                         .padding(.vertical, 1) // 왜인지는 모르지만 영역 넘치는 문제를 해결해주니 놔둘 것..
+                        .overlay {
+                            LinearGradient(
+                                colors: [.black.opacity(0), .black],
+                                startPoint: .init(x: 0.5, y: 0.8),
+                                endPoint: .bottom)
+                            .allowsHitTesting(false)
+                        }
                         .refreshable {
                             viewStore.send(.fetchDailyTests)
                         }
@@ -57,23 +64,22 @@ public struct HomeView: View {
                     
                     VStack {
                         Spacer()
-                        
-                        if !hideButton {
-                            bottomButton(isSolved: isSolvedTest) {
-                                @Dependency(\.shortUrlAPIManager) var shortURLAPIManager
-
-                                if isSolvedTest {
-                                    let url = "https://keyme-frontend.vercel.app/test/\(viewStore.testId)"
-                                    let shortURL = try await shortURLAPIManager.request(
-                                        .shortenURL(longURL: url),
-                                        object: BitlyResponse.self).link
-
-                                    sharedURL = ActivityViewController.SharedURL(shortURL)
-                                } else {
-                                    viewStore.send(.startTest(.presented(.startButtonDidTap)))
-                                }
+                        //                        if !hideButton {
+                        bottomButton(isSolved: isSolvedTest) {
+                            @Dependency(\.shortUrlAPIManager) var shortURLAPIManager
+                            
+                            if isSolvedTest {
+                                let url = "https://keyme-frontend.vercel.app/test/\(viewStore.testId)"
+                                let shortURL = try await shortURLAPIManager.request(
+                                    .shortenURL(longURL: url),
+                                    object: BitlyResponse.self).link
+                                
+                                sharedURL = ActivityViewController.SharedURL(shortURL)
+                            } else {
+                                viewStore.send(.startTest(.presented(.startButtonDidTap)))
                             }
                         }
+                        //                        }
                     }
                     .padding(.bottom, 26)
                 } else {
@@ -130,7 +136,7 @@ extension HomeView {
                 .cornerRadius(16)
                 .foregroundColor(DSKitAsset.Color.keymeWhite.swiftUIColor)
 
-            Text.keyme(isSolved ? "친구에게 공유하기" : "시작하기", font: .body2)
+            Text.keyme(isSolved ? "테스트 공유하기" : "시작하기", font: .body2)
                 .foregroundColor(.black)
         }
         .padding(.horizontal, 16)
